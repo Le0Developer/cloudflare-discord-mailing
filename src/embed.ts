7178dc9197029980fe4fece1c3b4f9a8ab6f6d8a
Jonathan Debétaz <contact@jonathan-d.ch>
2023-06-25 15:12:46 +0200

    Send parsed HTML when there is no textual content

--- src/embed.ts
+++ src/embed.ts
@@ -5,7 +5,7 @@ import { EmbedMail, PostalMail } from "./types"
 export function makeEmbed(email: PostalMail): APIEmbed {
 	return {
 		title: email.subject,
-		description: email.text.substring(0, 4096),
+		description: email.text.substring(0, 4096) || parseHTML(email.html).substring(0, 4096) || "No content found",
 		author: {
 			name: encodeAddress(email.from)
 		},
@@ -25,4 +25,19 @@ export function parseEmbed(embed: APIEmbed): EmbedMail {
 		from: decodeAddress(embed.author?.name as string),
 		timestamp: embed.timestamp as string,
 	}
+}
+
+function parseHTML(html: string): string {
+	html = html.replace(/<!doctype.*>/i,"");
+	html = html.replace(/<head>[\w\W]*?<\/head>/i,"");
+	html = html.replace(/<style.*>[\w\W]*?<\/style>/gi,"");
+	html = html.replace(/<script.*>[\w\W]*?<\/script>/gi,"");
+	html = html.replace(/\n|\r/g," ");
+	html = html.replace(/&\w+;/g," ");
+	html = html.replace(/\<\/p\>/gi,"\n");
+	html = html.replace(/\<\/li\>/gi," ");
+	html = html.replace(/\<br\s*?\/?\>/gi,"\n");
+	html = html.replace(/([ \t])+/g," ");
+	html = html.replace(/\n /g,"\n");
+	return html.replace(/(<([^>]+)>)/gi, "")
 }
