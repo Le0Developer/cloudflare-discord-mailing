161940d33e33b87dfbd84e3ffde0799961e5a61f
Jonathan Debétaz <contact@jonathan-d.ch>
2023-06-26 12:36:31 +0200

    Update src/embed.ts
    Co-authored-by: Leo Developer <LeoDeveloper@protonmail.com>

--- src/embed.ts
+++ src/embed.ts
@@ -34,7 +34,7 @@ function parseHTML(html: string): string {
 	html = html.replace(/<script[^>]*>.*?<\/script>/gi, "");
 	html = html.replace(/\n|\r/g, " ");
 	html = html.replace(/&#?\w+;/g, " ");
-	html = html.replace(/\<br\s*?\/?\>/gi,"\n");
+	html = html.replace(/<br\s*?\/?>/gi, "\n");
 	html = html.replace(/([ \t])+/g," ");
 	html = html.replace(/\n /g,"\n");
 	return html.replace(/(<([^>]+)>)/gi, "")
